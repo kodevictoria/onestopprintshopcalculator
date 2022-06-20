34af48893f02b0784bda06f38c79c24a97de5a93
Slingshot <69320709+slingshotXD@users.noreply.github.com>
2022-06-20 15:13:55 +0100

    Add files via upload

---
+++ java/calculation.js
@@ -0,0 +1,35 @@
+var showWeb;
+
+function webLoader() {
+  showWeb = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myWeb").style.display = "block";
+}
+
+function calculatePriceIn(){
+  var inwidth = document.getElementById("job-widthin").value;
+  var inheight = document.getElementById("job-heightin").value;
+  var innumber = document.getElementById("innumber").value;
+  var inprice = inwidth*inheight*innumber*120/144;
+  
+  document.getElementById("calculated-price").innerHTML = inprice;
+}
+function calculatePriceFt(){
+  var ftwidth = document.getElementById("job-widthft").value;
+  var ftheight = document.getElementById("job-heightft").value;
+  var ftnumber = document.getElementById("ftnumber").value;
+  var ftprice = ftwidth*ftheight*ftnumber*120;
+  
+  document.getElementById("calculated-price").innerHTML = ftprice;
+}
+function resetUnits(){
+  document.getElementById("job-widthin").value = '';
+  document.getElementById("job-heightin").value = '';
+  document.getElementById("innumber").value = '';
+  document.getElementById("job-widthft").value = '';
+  document.getElementById("job-heightft").value = '';
+  document.getElementById("ftnumber").value = '';
+}
